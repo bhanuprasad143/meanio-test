0672ea97c5b6f09764580c289e0cf55858c36d89
Tin Le <trongtin87@gmail.com>
2014-07-31 15:00:55 +0700

    display cart

--- packages/products/public/controllers/cart.js
+++ packages/products/public/controllers/cart.js
@@ -5,5 +5,12 @@ angular.module('mean.products').controller('CartController',
   function($scope, $stateParams, $location, Global, Products, shoppingCart) {
     $scope.global = Global;
     $scope.shoppingCart = shoppingCart;
+    $scope.shown = false;
+    $scope.toogleCartPanel = function(){
+    	$scope.shown = ! $scope.shown;
+    	console.log($scope.shoppingCart.items.keys);
+    	console.log($scope.shown);
+    };
+
   }
 ]);    
